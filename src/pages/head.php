3dab03b43088b7fc91a901b688a57de02785554c
supun-na97 <supunnanayakkara01@gmail.com>
2023-07-03 14:53:14 +0530

    Add logo

--- src/pages/head.php
+++ src/pages/head.php
@@ -7,13 +7,14 @@
                             allowed me to consider studying software engineering from an early age, and my enthusiasm
                             has perpetually developed since this time." />
     <meta name="keywords" content="Supun Nanayakkara, Supun, Ebats Technologies, Software Engineer, IT,
-                            Supun Nanayakkara Software Engineer, Senior Software Engineer, supun nanayakkara, Supun Srilanka, Traditionalme,
-                            traditionalme.life, union assurance plc, union assurance, union assurance supun" />
+                            Supun Nanayakkara Software Engineer, Senior Software Engineer, supun nanayakkara,
+                            Supun Srilanka, Traditionalme, traditionalme.life, union assurance plc, union assurance,
+                            union assurance supun" />
     <meta name="author" content="Supun Nanayakkara" />
     <!--  FavIcon  -->
-    <link rel="shortcut icon" href="assets/img/favicon.png">
+    <link rel="shortcut icon" href="assets/img/app-logo.png">
     <!-- Search Engine Crawler Tags -->
-    <meta name="robots" content="index,follow" /> 
+    <meta name="robots" content="index,follow" />
     <meta name="googlebot" content="index,follow" />
     <!-- Google Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Muli:300,400,600,700" rel="stylesheet">
